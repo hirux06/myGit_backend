e6fa899bf5b457d3521cdaa44d0b49c86c94d0dc
Saran Hiruthik <156305967+hirux06@users.noreply.github.com>
2025-02-09 12:27:57 +0530

    added issue, main, repo and user related files

---
+++ routes/repo.router.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const repoController = require("../controllers/repoController");
+
+const repoRouter = express.Router();
+
+
+repoRouter.post("/repo/create", repoController.createRepository);
+repoRouter.get("/repo/all", repoController.getAllRepositories);
+repoRouter.get("/repo/:id", repoController.fetchRepositoryById);
+repoRouter.get("/repo/name/:name", repoController.fetchRepositoryByName);
+repoRouter.get("/repo/user/:userID", repoController.fetchRepositoryForCurrentUser);
+repoRouter.put("/repo/update/:id", repoController.updateRepositoryById);
+repoRouter.patch("/repo/update/:id", repoController.toggleVisibilityById);
+repoRouter.delete("/repo/delete/:id", repoController.deleteRepositoryById);
+
+module.exports = repoRouter;
